5751109647fcf4726171645c59471804454a6f79
ledxdd <oVladid11@gmail.com>
2026-02-13 20:14:34 +0300

    added constructors and default sorting by id

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 import java.util.TreeSet;
 
 public class Main {
-    static public class SpaceMarine {
+    static public class SpaceMarine implements Comparable<SpaceMarine> {
         private Long id; //Поле не может быть null, Значение поля должно быть больше 0, Значение этого поля должно быть уникальным, Значение этого поля должно генерироваться автоматически
         private String name; //Поле не может быть null, Строка не может быть пустой
         private Coordinates coordinates; //Поле не может быть null
@@ -16,6 +16,40 @@ public class Main {
         private AstartesCategory category; //Поле может быть null
         private Weapon weaponType; //Поле может быть null
         private Chapter chapter; //Поле не может быть null
+
+        static private long next_id = 0L;
+
+        private void setName(String name) {
+            if (name == null || " ".equals(name) || name.trim().isEmpty()) {
+                System.out.println("Name can't be empty. Please enter the valid name!");
+            } else {
+                this.name = name;
+            }
+        }
+
+        public SpaceMarine() {
+            this.id = next_id++;
+        }
+
+        public SpaceMarine(String name, Coordinates crdnt, double health, String achievements, AstartesCategory category, Weapon weapon, Chapter chapter) {
+            this.id = next_id++;
+            setName(name);
+            this.coordinates.x = crdnt.x;
+            this.coordinates.y = crdnt.y;
+            this.creationDate = java.time.ZonedDateTime.now();
+            this.health = health;
+            this.achievements = achievements;
+            this.category = category;
+            this.weaponType = weapon;
+            this.chapter = chapter;
+        }
+
+        @Override
+        public int compareTo(SpaceMarine other) {
+            return this.id.compareTo(other.id);
+        }
+
+
     }
 
     static public class Coordinates {
@@ -27,6 +61,12 @@ public class Main {
         private String name; //Поле не может быть null, Строка не может быть пустой
         private int marinesCount; //Значение поля должно быть больше 0, Максимальное значение поля: 1000
         private String world; //Поле может быть null
+
+        Chapter(String name, int marinescnt, String wrld) {
+            this.name = name;
+            this.marinesCount = marinescnt;
+            this.world = wrld;
+        }
     }
 
     public enum AstartesCategory {
@@ -46,6 +86,7 @@ public class Main {
     static private TreeSet<SpaceMarine> collection;
     static private BufferedReader bReader;
     static private BufferedWriter bWriter;
+    static private Scanner global_scanner;
 
     static public void printHelp() {
         System.out.println("info : вывести в стандартный поток вывода информацию о коллекции (тип, дата инициализации, количество элементов и т.д.)");
@@ -77,57 +118,84 @@ public class Main {
     }
 
     static void clearCollection(TreeSet<SpaceMarine> collection) {
-        collection.clear();
+        if (!collection.isEmpty()) {
+            collection.clear();
+            System.out.println("Collection cleared!");
+        } else {
+            System.out.println("Nothing to clear. Collection is already expty!");
+        }
     }
 
     static void runProgramm(TreeSet<SpaceMarine> collection) {
         boolean is_running = true;
-        try (Scanner scanner = new Scanner(System.in)) {
-            System.out.println("Enter 'help' to see available commands:");
-            while (is_running)
-            {
-                System.out.print('>');
-                
-                String user_input;
-                user_input = scanner.nextLine();
-                
-                if (null != user_input) switch (user_input) {
-                    case "exit" -> is_running = false;
-                    case "help" -> printHelp();
-                    case "clear()" -> {
-                        for (int i = 0; i < 1200; i++) {
-                            System.out.println(" ");
-                        }
-                    }
-                    case "average_of_health" -> {
-                        double av_health = getAvHealth();
-                        System.out.println(av_health);
-                    }
-                    case "clear" -> clearCollection(collection);
-                    default -> {
+        
+        System.out.println("Enter 'help' to see available commands:");
+        while (is_running)
+        {
+            System.out.print('>');
+            
+            
+            String user_input;
+            user_input = global_scanner.nextLine();
+            
+            if (null != user_input) switch (user_input) {
+                case "exit" -> is_running = false;
+                case "help" -> printHelp();
+                case "clear()" -> {
+                    for (int i = 0; i < 1200; i++) {
+                        System.out.println(" ");
                     }
                 }
-            } 
-        }
+                case "average_of_health" -> {
+                    double av_health = getAvHealth();
+                    System.out.println(av_health);
+                }
+                case "clear" -> clearCollection(collection);
+                case "add" -> fillMarine();
+                default -> {
+                }
+            }
+        } 
+        
     }
 
-    static void fillMarine(SpaceMarine marine) {
+    static boolean checkName(String name) {
+        return !(name == null || " ".equals(name) || name.trim().isEmpty());
+    }
 
+    static void fillMarine() {
+        SpaceMarine marine = new SpaceMarine();
+        
+        System.out.println("*******| New marine creation |*******");
+        
+        boolean creating_marine = true;
+        
+        while (creating_marine) {
+            System.out.println("Enter the marine name: ");
+            String name = global_scanner.nextLine();
+            
+            if (checkName(name)) {
+                marine.setName(name);
+                creating_marine = false;
+            } else {
+                System.out.println("Name can't be blank. Please, enter again: ");
+            }
+            
+        }
+        
     }
 
     static void fillCollection(TreeSet<SpaceMarine> collection) {
         boolean collecting_info = true;
 
         while (collecting_info) {
-            SpaceMarine space_marine = new SpaceMarine();
-
-            fillMarine(space_marine);
+            
         }
     }
 
     public static void main(String[] args) {
         collection = new TreeSet<>();
-        fillCollection(collection);
+        global_scanner = new Scanner(System.in);
         
         runProgramm(collection);
     }
